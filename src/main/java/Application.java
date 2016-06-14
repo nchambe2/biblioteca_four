1e159725f91c3cdf508a3c313df67ca50f7dffff
Nicolette Chambers <nicolette3883@gmail.com>
2016-06-14 14:29:18 -0500

    #1 | When application starts Welcome to Biblioteca message will be displayed

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+
+public class Application {
+    private PrintStream printStream;
+
+    public Application(PrintStream printStream) {
+
+        this.printStream = printStream;
+    }
+
+    public void start() {
+        printStream.println("Welcome to Biblioteca");
+    }
+}
