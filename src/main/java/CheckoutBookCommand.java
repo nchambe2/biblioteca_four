1a8098eb0b27beaff791dd338e6414732e1634ee
Nicolette Chambers <nicolette3883@gmail.com>
2016-06-21 11:48:42 -0500

    #7 | Create checkoutbook command and implement name method

---
+++ src/main/java/CheckoutBookCommand.java
@@ -0,0 +1,5 @@
+/**
+ * Created by nicollettec on 6/21/16.
+ */
+public class CheckoutBookCommand {
+}
---
+++ src/test/java/CheckoutBookCommandTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by nicollettec on 6/21/16.
+ */
+public class CheckoutBookCommandTest {
+
+}
